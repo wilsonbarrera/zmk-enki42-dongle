2557fb7b2a183404c5cab695e454f38da6ab481b
Wilson Barrera Jr <contacto@wilsonbarrera.com>
2024-03-08 12:33:10 -0500

    Update simbolos y numeros arreglados keymap_german_mac_iso_zmk.h

--- config/boards/shields/enki42/keymap_german_mac_iso_zmk.h
+++ config/boards/shields/enki42/keymap_german_mac_iso_zmk.h
@@ -140,7 +140,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 // Row 1
 #define DE_CARET &kp LS(LA(N6)) // ^
-#define DE_CIRC &kp GRAVE // ^ (dead)
+#define DE_CIRC &kp GRAVE // TILDE (dead)
 #define DE_1 &kp N1       // 1
 #define DE_2 &kp N2       // 2
 #define DE_3 &kp N3       // 3
@@ -151,21 +151,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DE_8 &kp N8       // 8
 #define DE_9 &kp N9       // 9
 #define DE_0 &kp N0       // 0
-#define DE_SS &kp MINUS   // ß
-#define DE_ACUT &kp EQUAL // ´ (dead)
+#define DE_SS &kp MINUS   // SIGNO DE MENOS -
+#define DE_ACUT &kp EQUAL // ´ (dead) SIGNO IGUAL
 // Row 2
 #define DE_Q &kp Q       // Q
 #define DE_W &kp W       // W
 #define DE_E &kp E       // E
 #define DE_R &kp R       // R
 #define DE_T &kp T       // T
-#define DE_Z &kp Y       // Z
+#define DE_Y &kp Y       // Y
 #define DE_U &kp U       // U
 #define DE_I &kp I       // I
 #define DE_O &kp O       // O
 #define DE_P &kp P       // P
-#define DE_UDIA &kp LBKT // Ü
-#define DE_PLUS &kp RBKT // +
+#define DE_UDIA &kp LBKT // left bracket[
+#define DE_PLUS &kp RBKT // rigth braket ]
 // Row 3
 #define DE_A &kp A            // A
 #define DE_S &kp S            // S
@@ -176,12 +176,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DE_J &kp J            // J
 #define DE_K &kp K            // K
 #define DE_L &kp L            // L
-#define DE_ODIA &kp SEMICOLON // Ö
-#define DE_ADIA &kp APOS      // Ä
+#define DE_ODIA &kp SEMICOLON // ;
+#define DE_ADIA &kp APOS      // apostrofe '
 #define DE_HASH &kp HASH      // #
 // Row 4
-#define DE_LABK &kp GRAVE // <
-#define DE_Y &kp Z        // Y
+#define DE_LABK &kp LESS_THAN // <
+#define DE_Z &kp Z        // Y
 #define DE_X &kp X        // X
 #define DE_C &kp C        // C
 #define DE_V &kp V        // V
@@ -190,7 +190,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DE_M &kp M        // M
 #define DE_COMM &kp COMMA // ,
 #define DE_DOT &kp DOT    // .
-#define DE_MINS &kp SLASH // -
+#define DE_SLASH &kp SLASH // /
 
 /* Shifted symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬─────┐
@@ -206,10 +206,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * └─────┴────┴─────┴───────────────────────┴─────┴────┴─────┘
  */
 // Row 1
-#define DE_DEG  &kp LS(GRAVE) // °
+#define DE_VIRGU  &kp LS(GRAVE) // VIRGURILLA
 #define DE_EXCL &kp LS(N1)    // !
 #define DE_DQUO &kp LS(N2)    // "
-#define DE_SECT &kp LS(N3)    // §
+#define DE_HASH &kp LS(N3)    // #
 #define DE_DLR  &kp LS(N4)    // $
 #define DE_PERC &kp LS(N5)    // %
 #define DE_AMPR &kp LS(N6)    // &
@@ -217,18 +217,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DE_LPRN &kp LS(N8)    // (
 #define DE_RPRN &kp LS(N9)    // )
 #define DE_EQL  &kp LS(N0)    // =
-#define DE_QUES &kp LS(MINUS)   // ?
-#define DE_GRV  &kp LS(EQUAL) // ` (dead)
+#define DE_QUES &kp QMARK   // ?
+//#define DE_GRV  &kp LS(EQUAL) // ` (dead)
 // Row 2
 #define DE_ASTR &kp LS(RBKT) // *
-// #define DE_STAR &kp RBRC // "*" DE_ASTR
+#define DE_STAR &kp ASTRK // "*" DE_ASTR
 // Row 3
-#define DE_QUOT &kp LS(BSLH) // '
+//#define DE_QUOT &kp LS(BSLH) // '
 // Row 4
-#define DE_RABK &kp LS(GRAVE) // >
-#define DE_SCLN &kp LS(COMMA) // ;
-#define DE_COLN &kp LS(DOT)  // :
-#define DE_UNDS &kp LS(SLASH) // _
+#define DE_RABK &kp GT // >
+//#define DE_SCLN &kp LS(COMMA) // ;
+#define DE_COLN &kp LS(SEMICOLON)  // :
+#define DE_UNDS &kp LS(MINUS) // _
 
 /* Alted symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬─────┐
@@ -378,7 +378,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define DE_AMPS &kp CARET         // "&" DE_AMPR
 // #define DE_BSLH &kp RA(MINUS)     // "\" DE_BSLS
 // #define DE_QUESTION &kp LS(MINUS) // ? DE_QUES
-// #define DE_AT &kp RA(Q)           // "@"
+// #define DE_AT &kp AT           // "@"
 // #define DE_LPAR &kp LS(N8)        // ( DE_LPRN
 // #define DE_RPAR &kp LS(N9)        // ) DE_RPRN
 // #define DE_LBKT &kp RA(N8)        // [ DE_LBRC
